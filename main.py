04fdfac0931ba356b27d55fad166a1c22e01c521
wooloves39 <wooloves@naver.com>
2016-12-07 13:14:02 +0900

    update study

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import game_framework
 import title
 import playboss
+import platform
+import os
+if platform.architecture()[0] == '32bit':
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
+else:
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
+
 from pico2d import *
 open_canvas()
 game_framework.run(title)
